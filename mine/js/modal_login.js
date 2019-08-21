e085b9d0d30ff5f4401afc4828cbb4ba1cc8831b
Patty <roxane.yps@gmail.com>
2019-08-21 23:59:52 +0800

    完成了登入註冊彈窗

--- mine/js/modal_login.js
+++ mine/js/modal_login.js
@@ -1,20 +1,14 @@
-function changeToMobileNum(){
-	var MobileNum=document.getElementById("MobileNum").className = "text-c fw-bold font-size-16 active";
-	var Username=document.getElementById("Username").className = "text-c fw-bold font-size-16";
-
-	var MobileLoginShow=document.getElementById("mobile_login");
-	mobile_login.style.display="block";
-	var UsernameLoginHide=document.getElementById("username_login");
-	username_login.style.display="block";
+function changeToLogin(){
+	var userLoginShow=document.getElementById("userLogin");
+	userLogin.style.display="block";
+	var userSignupHide=document.getElementById("userSignup");
+	userSignup.style.display="none";
 }
 
-function changeToUsername(){
-	var Username=document.getElementById("Username").className = "text-c fw-bold font-size-16 active";
-	var MobileNum=document.getElementById("MobileNum").className = "text-c fw-bold font-size-16";
-
-	var MobileLoginHide=document.getElementById("mobile_login");
-	mobile_login.style.display="block";
-	var UsernameLoginShow=document.getElementById("username_login");
-	username_login.style.display="block";
+function changeToSignUp(){
+	var userLoginHide=document.getElementById("userLogin");
+	userLogin.style.display="none";
+	var userSignupShow=document.getElementById("userSignup");
+	userSignup.style.display="block";
 }
 
