ce0f10665bd0a6e4439b4efc2dfa0a58c557dade
Patty <roxane.yps@gmail.com>
2019-08-30 22:46:35 +0800

    調整menu

--- mine/js/menu_header.js
+++ mine/js/menu_header.js
@@ -1,11 +1,15 @@
 function menuClose() {
 	var menuMask = document.getElementById("menu-r");
 	  if (menuMask.style.display === "none") {
-	  	document.getElementById("page-all").classList.add("scrollNone");
 	    menuMask.style.display = "block";
+	    /*
+	    document.getElementById("page-all").classList.add("scrollNone");
+	    */
 	    
 	  } else {
 	    menuMask.style.display = "none";
+	    /*
 	    document.getElementById("page-all").classList.remove("scrollNone");
+	    */
 	  };
 }
