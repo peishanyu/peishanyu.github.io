1f1d0233c27d243ab8d29423478fa7bd6ff154b9
Patty <roxane.yps@gmail.com>
2019-12-03 00:56:05 +0800

    開始加 popup

---
+++ mine/js/popup-cart.js
@@ -0,0 +1,26 @@
+function gotoCartModal() {
+
+	var gotoCartMask = document.getElementById("add-gotoCartMask");
+	if (gotoCartMask.style.display === "none") {
+		gotoCartMask.style.display = "block";
+	} else {
+		gotoCartMask.style.display = "none";
+	};
+
+	var gotoCartPopup = document.getElementById("gotoCartPopup");
+	if (gotoCartPopup.style.display === "none") {
+		gotoCartPopup.style.display = "block";
+	} else {
+		gotoCartPopup.style.display = "none";
+	};
+
+};
+
+
+
+
+
+
+
+
+
