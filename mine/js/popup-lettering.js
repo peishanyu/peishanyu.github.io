f117e34d1e9be53b1b1c6923d135827c50672927
Patty <roxane.yps@gmail.com>
2019-12-03 18:54:14 +0800

    回家繼續

---
+++ mine/js/popup-lettering.js
@@ -0,0 +1,26 @@
+function letteringModal() {
+
+	var letteringMask = document.getElementById("add-letteringMask");
+	if (letteringMask.style.display === "none") {
+		letteringMask.style.display = "block";
+	} else {
+		letteringMask.style.display = "none";
+	};
+
+	var letteringPopup = document.getElementById("letteringPopup");
+	if (letteringPopup.style.display === "none") {
+		letteringPopup.style.display = "block";
+	} else {
+		letteringPopup.style.display = "none";
+	};
+
+};
+
+
+
+
+
+
+
+
+
