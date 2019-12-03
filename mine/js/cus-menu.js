21192492642773a41123534c3ec27d7de2443406
Patty <roxane.yps@gmail.com>
2019-12-04 00:50:07 +0800

    新增了客製項目選單

---
+++ mine/js/cus-menu.js
@@ -0,0 +1,19 @@
+function cusMenuModal() {
+
+	var cusMenuPopup = document.getElementById("cusMenu");
+	if (cusMenuPopup.style.display === "none") {
+		cusMenuPopup.style.display = "block";
+	} else {
+		cusMenuPopup.style.display = "none";
+	};
+
+};
+
+
+
+
+
+
+
+
+
