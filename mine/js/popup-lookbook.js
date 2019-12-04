c8e1c5e32258ae982d37c3a1316a0a28af84b3d9
Patty <roxane.yps@gmail.com>
2019-12-04 17:09:50 +0800

    回家繼續

---
+++ mine/js/popup-lookbook.js
@@ -0,0 +1,26 @@
+function lookbookModal() {
+
+	var lookbookMask = document.getElementById("add-lookbookMask");
+	if (lookbookMask.style.display === "none") {
+		lookbookMask.style.display = "block";
+	} else {
+		lookbookMask.style.display = "none";
+	};
+
+	var lookbookPopup = document.getElementById("lookbookPopup");
+	if (lookbookPopup.style.display === "none") {
+		lookbookPopup.style.display = "block";
+	} else {
+		lookbookPopup.style.display = "none";
+	};
+
+};
+
+
+
+
+
+
+
+
+
