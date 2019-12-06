2532bbbb8a585504214f1f8c66696d2dfced9c06
Patty <roxane.yps@gmail.com>
2019-12-06 18:33:29 +0800

    新增了花費時間提示框
    待調整

---
+++ mine/js/popup-finished.js
@@ -0,0 +1,26 @@
+function cusFinishedModal() {
+
+	var cusFinishedMask = document.getElementById("add-cusFinishedMask");
+	if (cusFinishedMask.style.display === "none") {
+		cusFinishedMask.style.display = "block";
+	} else {
+		cusFinishedMask.style.display = "none";
+	};
+
+	var cusFinishedPopup = document.getElementById("cusFinishedPopup");
+	if (cusFinishedPopup.style.display === "none") {
+		cusFinishedPopup.style.display = "block";
+	} else {
+		cusFinishedPopup.style.display = "none";
+	};
+
+};
+
+
+
+
+
+
+
+
+
