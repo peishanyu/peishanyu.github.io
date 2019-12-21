384859bc36f07e21fa2039cf377218de475aa98d
Patty <roxane.yps@gmail.com>
2019-12-21 11:45:13 +0800

    選擇縣市地區的popup

---
+++ mine/js/list-city.js
@@ -0,0 +1,21 @@
+
+function modalListCity() {
+  var x = document.getElementById("add-city-modalMask");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  };
+
+var y = document.getElementById("modalListCity");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  };	  
+
+};
+
+
+
+
---
+++ mine/js/list-zone.js
@@ -0,0 +1,22 @@
+
+function modalListZone() {
+  var x = document.getElementById("add-zone-modalMask");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  };
+
+var y = document.getElementById("modalListZone");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  };	  
+
+};
+
+
+
+
+
