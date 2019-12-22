22bca093d31ddea437bf488e78e54cc6bc7a8157
Patty <roxane.yps@gmail.com>
2019-12-22 16:50:18 +0800

    客製化頁面 新增預覽彈窗

---
+++ mine/js/popup-preview.js
@@ -0,0 +1,26 @@
+function previewModal() {
+
+	var previewMask = document.getElementById("add-previewMask");
+	if (previewMask.style.display === "none") {
+		previewMask.style.display = "block";
+	} else {
+		previewMask.style.display = "none";
+	};
+
+	var previewPopup = document.getElementById("previewPopup");
+	if (previewPopup.style.display === "none") {
+		previewPopup.style.display = "block";
+	} else {
+		previewPopup.style.display = "none";
+	};
+
+};
+
+
+
+
+
+
+
+
+
