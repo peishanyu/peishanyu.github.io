bb8e1d0035fb0123aace10d9e150e59c98854c9d
Patty <roxane.yps@gmail.com>
2019-12-23 12:35:20 +0800

    新增了 radio & checkbox 效果

---
+++ mine/js/list-recentAdd.js
@@ -0,0 +1,21 @@
+
+function modalListRecentAdd() {
+  var x = document.getElementById("recent-add-modalMask");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  };
+
+var y = document.getElementById("modalrecentAdd");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  };	  
+
+};
+
+
+
+
