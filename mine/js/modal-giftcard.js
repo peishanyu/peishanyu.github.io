d6e5d0a2351bca1dfaffc2b93c46ea566f174b31
Patty <roxane.yps@gmail.com>
2019-12-26 18:44:43 +0800

    新增了禮物popup

---
+++ mine/js/modal-giftcard.js
@@ -0,0 +1,26 @@
+function modalGiftCard() {
+
+	var giftcardMask = document.getElementById("add-giftcardMask");
+	if (giftcardMask.style.display === "none") {
+		giftcardMask.style.display = "block";
+	} else {
+		giftcardMask.style.display = "none";
+	};
+
+	var modalGiftcard = document.getElementById("modal-giftcard");
+	if (modalGiftcard.style.display === "none") {
+		modalGiftcard.style.display = "block";
+	} else {
+		modalGiftcard.style.display = "none";
+	};
+
+};
+
+
+
+
+
+
+
+
+
