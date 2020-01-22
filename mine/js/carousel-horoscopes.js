16704106d352b4ed3a9465b67d5d67d2649ae0e9
Patty <roxane.yps@gmail.com>
2020-01-22 18:34:06 +0800

    星座運勢的回馬槍 彈窗

---
+++ mine/js/carousel-horoscopes.js
@@ -0,0 +1,26 @@
+function carouselHoroscopesModal() {
+
+	var prdCarouselMask = document.getElementById("add-prd-carousel-Mask");
+	if (prdCarouselMask.style.display === "none") {
+		prdCarouselMask.style.display = "block";
+	} else {
+		prdCarouselMask.style.display = "none";
+	};
+
+	var modal_prdCarousel = document.getElementById("modal_carousel");
+	if (modal_prdCarousel.style.display === "none") {
+		modal_prdCarousel.style.display = "block";
+	} else {
+		modal_prdCarousel.style.display = "none";
+	};
+
+};
+
+
+
+
+
+
+
+
+
